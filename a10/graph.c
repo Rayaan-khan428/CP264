7e327161f6ee7940b90624cee0ce32cf61a17862
Rayaan Khan <rayaan.k.ca@gmail.com>
2023-07-29 19:47:31 -0400

    finished q2

--- a10/graph.c
+++ a10/graph.c
@@ -8,19 +8,147 @@
 #include "graph.h"
 
 GRAPH *new_graph(int order) {
-// your implementation
+  // your implementation
+  int i = 0;
+
+	GRAPH *p = malloc(sizeof(GRAPH));
+
+	p->order = order;
+	p->size = 0;
+
+	p->nodes = malloc(order * sizeof(GNODE*));
+
+	for(i = 0; i < order; i += 1){
+		p->nodes[i] = malloc(sizeof(GNODE));
+		p->nodes[i]->neighbor = NULL;
+		p->nodes[i]->nid = i;
+	}
+
+	return p;
 }
 
 void add_edge(GRAPH *g, int from, int to, int weight) {
-// your implementation
+  // your implementation
+  int found = 0;
+	ADJNODE *p = g->nodes[from]->neighbor;
+
+	if(p){
+
+	while(p->next){
+
+	    if(p->nid == to){
+	      found = 1;
+	      break;
+	    }
+
+	    p = p->next;
+
+	  }
+
+	  if(p->nid == to){
+	      found = 1;
+	  }
+
+	  if(found){
+	    p->weight = weight;
+	  }
+	  else {
+	    ADJNODE *n = malloc(sizeof(ADJNODE));
+	    n->next = NULL;
+	    n->nid = to;
+	    n->weight = weight;
+	    p->next = n;
+	  }
+	}
+	else {
+	  ADJNODE *n = malloc(sizeof(ADJNODE));
+	  n->next = NULL;
+	  n->nid = to;
+	  n->weight = weight;
+	  g->nodes[from]->neighbor = n;
+	}
+
+	g->size += 1;
 }
 
 void display_bforder(GRAPH *g, int start) {
-// your implementation
+  
+  // your implementation
+	if (g == NULL){
+		return;
+	}
+
+	int n = g->order, visited[n], i;
+	for (i = 0; i < n; i += 1) {
+		visited[i] = 0;
+	}
+
+	QUEUE queue = {0};
+	GNODE *gnp = NULL;
+	ADJNODE *anp = NULL;
+
+	enqueue(&queue, g->nodes[start]);
+
+	visited[start] = 1;
+
+	while (queue.front) {
+	    gnp = (GNODE*) dequeue(&queue);
+	    printf("%d ", gnp->nid);
+
+	    ADJNODE *p = gnp->neighbor;
+
+	    while(p){
+
+	      if(!visited[p->nid]){
+
+	        enqueue(&queue, g->nodes[p->nid]);
+	        visited[p->nid] = 1;
+	      }
+
+	      p = p->next;
+
+	    }
+	  }
+
+	clean_queue(&queue);
+
 }
 
 void display_dforder(GRAPH *g, int start) {
-// your implementation
+  // your implementation
+  if (g == NULL) {
+		return;
+	}
+
+	int n = g->order, visited[n], i;
+
+	for (i = 0; i < n; i += 1){
+		visited[i] = 0;
+	}
+
+	STACK stack = {0};
+	GNODE *gnp = NULL;
+	ADJNODE *anp = NULL;
+
+	push(&stack, g->nodes[start]);
+
+	visited[start] = 1;
+
+	while (stack.top) {
+	    gnp = (GNODE*) stack.top->data;
+	    pop(&stack);
+	    printf("%d ", gnp->nid);
+
+	    ADJNODE *p = gnp->neighbor;
+	    while(p){
+	      if(!visited[p->nid]){
+	        push(&stack, g->nodes[p->nid]);
+	        visited[p->nid] = 1;
+	      }
+	    p = p->next;
+	    }
+	  }
+	clean_stack(&stack);
 }
 
 int get_weight(GRAPH *g, int from, int to) {
